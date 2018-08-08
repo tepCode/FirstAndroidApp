06343a960c5c92072a852ad538e5492c489977aa
tepCode <felixteppe8@gmail.com>
2018-08-08 20:07:50 +0200

    Hello World, does it work?

---
+++ FirstAndroidApp/src/de/teppe/first_android_app/HelloWorld.java
@@ -0,0 +1,9 @@
+package de.teppe.first_android_app;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Does it work?");
+	}
+}
